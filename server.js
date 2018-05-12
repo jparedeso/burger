e206a7b2f7705b4dd55fb6bdae9adc10f5f61d17
Jorge Paredes <jparedeso@gmail.com>
2018-05-11 20:58:39 -0400

    Setting up server.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+const db = require("./models");
+
+app.engine('.hbs', expressHbs({
+    defaultLayout: "layout",
+    extname: ".hbs"
+}));
+app.set("view engine", "hbs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
